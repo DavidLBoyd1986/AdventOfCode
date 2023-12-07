0040de864cefe1d9eb872e60ddbdd8d30215eb9f
David <davidlboyd1986@gmail.com>
2023-12-06 22:31:39 -0500

    Clean up part 1 complete, more to refactor

--- 2023/DayThree.py
+++ 2023/DayThree.py
@@ -15,128 +15,104 @@ with open(pathToExample, 'r') as f:
 
 def problem_one(test_data):
     total = 0
-    print(test_data)
     # Get size of grid:
     grid_length = len(test_data[0])-1
     grid_height = len(test_data)
     grid_size = (grid_length, grid_height)
     print(grid_size)
+    print('grid_length = ' + str(grid_length))
     # list of parts, a data structure with all required part information
     parts_list = []
-    # will hold the digits of the value of the part being looked at on the schematic
-    part_value = ''
     # a list of symbols locations a tuple with (x,y) coordinates
     symbols_list = []
     row_number = 0
 
-    # Loop through rows and columns
+    # Loop through rows
     for row in test_data:
+        # Lists of parts and symbols to hold for this row, and append to main lists after row has been processed.
         row_parts = []
         row_symbols = []
+        # Part initialization values
         part = {}
-        value_length = 0
-        column_number = 0
-        valid_part = False
-        previous_character = 'empty'
         part_value = ''
+        value_length = 0
+        valid_part = False
+        # Grid tracking values
+        column_number = 0
+        previous_character = 'empty'
 
-        for column in row:
-            if column == '.':
-                if previous_character == 'empty':
-                    previous_character = 'empty'
-                    column_number += 1
-                    continue
-                if previous_character == 'symbol':
-                    previous_character = 'empty'
-                    column_number += 1
-                    continue
+        # Loop through columns
+        for current_character in row:
+            # Handle empty characters
+            if current_character == '.':
                 if previous_character == 'digit':
-                    previous_character = 'empty'
                     # set length of part value and the complete value
                     part['length'] = value_length
                     part['value'] = part_value
                     row_parts.append(part)
                     # reset part, value_length, and part_value
-                    part = {}
-                    value_length = 0
-                    part_value = ''
-                    column_number += 1
-                    continue
+                    part, value_length, part_value = reset_part()
+                previous_character = 'empty'
+                column_number += 1
+                continue
 
-            if column in string.punctuation:
-                # since it's a symbol, look in row above for digits to make their part invalid
+            if current_character in string.punctuation:
+                # since it's a symbol, look in row above for digits to make their part valid
                 if row_number > 0:
                     look_above_for_digits(parts_list[row_number - 1], column_number)
-                symbol = (row_number, column_number)
-                row_symbols.append(symbol)
+                # Add symbol to row_symbol list
+                row_symbols.append((row_number, column_number))
 
-                if previous_character == 'empty':
-                    previous_character = 'symbol'
-                    column_number += 1
-                    continue
-                if previous_character == 'symbol':
-                    previous_character = 'symbol'
-                    column_number += 1
-                    continue
                 if previous_character == 'digit':
-                    previous_character = 'symbol'
-                    # set length of part value, the complete value, and that it's invalid
+                    # set length of part value, the complete value, and that it's valid
                     part['length'] = value_length
                     part['value'] = part_value
                     part['valid'] = True
                     row_parts.append(part)
                     # reset part, value_length, and part_value
-                    part = {}
-                    value_length = 0
-                    part_value = ''
-                    column_number += 1
-                    continue
+                    part, value_length, part_value = reset_part()
+                previous_character = 'symbol'
+                column_number += 1
+                continue
 
-            if column in string.digits:
-                # since it's a digit, look in row above for symbols to make this part invalid
+            if current_character in string.digits:
+                # since it's a digit, look in row above for symbols to make this part valid
                 if row_number > 0:
                     valid_part = look_above_for_symbols(symbols_list[row_number - 1], column_number)
 
-                # If currently on a valid part, check above for symbols
                 if previous_character == 'empty':
-                    previous_character = 'digit'
                     # row and column are done, 'length', 'value', and 'valid' can change still
                     part = {'row': row_number, 'column': column_number, 'length': 0, 'value': 0, 'valid': False}
                     if valid_part:
                         part['valid'] = True
                     value_length += 1
-                    part_value += column
+                    part_value += current_character
                     if column_number >= grid_length-1:
                         part['value'] = part_value
                         part['length'] = value_length
                         row_parts.append(part)
-                    column_number += 1
-                    continue
-                if previous_character == 'digit':
+                elif previous_character == 'digit':
                     if valid_part:
                         part['valid'] = True
-                    previous_character = 'digit'
                     value_length += 1
-                    part_value += column
+                    part_value += current_character
                     if column_number >= grid_length-1:
                         part['value'] = part_value
                         part['length'] = value_length
                         row_parts.append(part)
-                    column_number += 1
-                    continue
-                if previous_character == 'symbol':
-                    previous_character = 'digit'
+                elif previous_character == 'symbol':
                     part = {'row': row_number, 'column': column_number, 'length': 0, 'value': 0, 'valid': True}
                     if valid_part:
                         part['valid'] = True
                     value_length += 1
-                    part_value += column
+                    part_value += current_character
                     if column_number >= grid_length-1:
                         part['value'] = part_value
                         part['length'] = value_length
                         row_parts.append(part)
-                    column_number += 1
-                    continue
+                previous_character = 'digit'
+                column_number += 1
+                continue
         # Troubleshooting step, checking for 0 part values
         for part in row_parts:
             if part['value'] == 0:
@@ -152,7 +128,7 @@ def problem_one(test_data):
         for part in row:
             if part['valid']:
                 total += int(part['value'])
-
+    # Trouble Shooting step
     for row in symbols_list:
         print(row)
 
@@ -162,6 +138,13 @@ def problem_one(test_data):
 def problem_two(test_data):
     total = 0
     return total
+
+
+def reset_part():
+    part = {}
+    value_length = 0
+    part_value = ''
+    return part, value_length, part_value
 
 
 def look_above_for_symbols(row_symbols, current_column):
